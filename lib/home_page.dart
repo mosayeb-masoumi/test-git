5a83ebf05ce89e9066286b05a4502a6c83f87547
m-masoumi <m-masoumi@agri-bank.com>
2024-03-16 16:23:44 +0330

    HomePage: added space

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,6 +20,7 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             ElevatedButton(onPressed: (){}, child: Text("btn")),
+            //space
           ],
         ),
       ),
