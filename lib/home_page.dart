9de8b6a9b2464dcc7d86b7e3ebe8d932780877d0
m-masoumi <m-masoumi@agri-bank.com>
2024-05-20 14:54:07 +0330

    Revert "added items in feature1"
    This reverts commit 9407e48f01a751ff50f7f3e0dc3809cd9d2467c8.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -25,9 +25,6 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             ElevatedButton(onPressed: (){}, child: Text("btn")),
-
-            ElevatedButton(onPressed: (){}, child: Text("btn f1")),
-            ElevatedButton(onPressed: (){}, child: Text("btn f1")),
           ],
 
         ),
