3b8d782d3e918d07a88fe5280b46195144ab4c09
m-masoumi <m-masoumi@agri-bank.com>
2024-05-20 14:58:27 +0330

    ADDED F2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -25,6 +25,8 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             ElevatedButton(onPressed: (){}, child: Text("btn")),
+
+            ElevatedButton(onPressed: (){}, child: Text("btn f2")),
           ],
 
         ),
